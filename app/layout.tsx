de3d34bb7c52bbed52d2fdcb4573762fca0be915
bruxindu7 <bruxinsete@gmail.com>
2025-09-16 17:27:11 -0300

    Atualização Pixel Facebook (769761439287491)

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '769761439287491');
+              fbq('init', '1340800104280046');
               fbq('track', 'PageView');
             `,
           }}
@@ -54,7 +54,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=769761439287491&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1340800104280046&ev=PageView&noscript=1"
             alt="fb-pixel"
           />
         </noscript>
