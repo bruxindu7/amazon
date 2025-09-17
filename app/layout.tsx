2b4d7b4991345796c4f986cd21a962e808243c75
bruxindu7 <bruxinsete@gmail.com>
2025-09-17 20:46:23 -0300

    Substituído Meta Pixel (Facebook) pelo Google Ads Tag (gtag.js) no RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,38 +26,25 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        {/* Meta Pixel Code */}
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17550554496"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1340800104280046');
-              fbq('track', 'PageView');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17550554496');
             `,
           }}
         />
-        {/* End Meta Pixel Code */}
+        {/* End Google tag */}
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
-
-        {/* Noscript precisa estar no body */}
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1340800104280046&ev=PageView&noscript=1"
-            alt="fb-pixel"
-          />
-        </noscript>
       </body>
     </html>
   );
